05ea6b2645cb884b974f365bd90a1386669ddd33
lukewis <lukewis@users.noreply.github.com>
2018-12-20 10:13:04 -0500

    - Changing module type to umd

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,5 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.helloWorld = function () {
-    console.info("Hello world");
-};
+(function (factory) {
+    if (typeof module === "object" && typeof module.exports === "object") {
+        var v = factory(require, exports);
+        if (v !== undefined) module.exports = v;
+    }
+    else if (typeof define === "function" && define.amd) {
+        define(["require", "exports"], factory);
+    }
+})(function (require, exports) {
+    "use strict";
+    Object.defineProperty(exports, "__esModule", { value: true });
+    exports.helloWorld = function () {
+        console.info("Hello world");
+    };
+});
